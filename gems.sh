706c7bed8a3abd752a6454c09f4997439dd90353
Colin Jones <trptcolin@gmail.com>
2014-01-22 15:05:55 -0600

    Specify gem versions; delay autorelease pool drain

--- gems.sh
+++ gems.sh
@@ -1,5 +1,5 @@
 TARGET_ROOT="./UniversalRuby"
 
 # Builds native gems, so needs to be run with a
-gem install eventmachine -i $TARGET_ROOT/lib/ruby/gems/2.1.0 --no-ri --no-rdoc
-gem install thin -i $TARGET_ROOT/lib/ruby/gems/2.1.0 --no-ri --no-rdoc
+gem install eventmachine -v 1.0.3 -i $TARGET_ROOT/lib/ruby/gems/2.1.0 --no-ri --no-rdoc
+gem install thin -v 1.4.1 -i $TARGET_ROOT/lib/ruby/gems/2.1.0 --no-ri --no-rdoc
--- main.m
+++ main.m
@@ -10,18 +10,18 @@ int main()
     NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
     chdir([resourcePath UTF8String]);
     NSString *mainPath = [[resourcePath stringByAppendingPathComponent:@"main.rb"] retain];
-    [pool drain];
-
     NSString* rubyExecutable = [[NSBundle mainBundle] pathForResource:@"ruby" ofType:nil];
 
     NSTask *task = [[NSTask alloc] init];
     [task setLaunchPath:rubyExecutable];
-    NSString* gemHome = [[NSBundle mainBundle] pathForResource:@"lib/ruby/gems/2.1.0" ofType:nil];
-    NSString* gemPath = [[NSBundle mainBundle] pathForResource:@"lib/ruby/gems/2.1.0" ofType:nil];
+    NSString* gemHome = [NSString stringWithFormat:@"%@/%@", resourcePath, @"lib/ruby/gems/2.1.0"];
+    NSString* gemPath = gemHome;
     [task setEnvironment:@{@"GEM_HOME": gemHome,
                            @"GEM_PATH": gemPath}];
+    // TODO: CWJ add PATH so projects can shell out (e.g. `ifconfig`)
     [task setArguments:[NSArray arrayWithObjects:@"main.rb", nil]];
 
     [task launch];
     [task waitUntilExit];
+    [pool drain];
 }
