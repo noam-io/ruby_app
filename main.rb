84e9b16fbdbb7fa98aa546a7d43444af77ffdacd
Colin Jones <trptcolin@gmail.com>
2014-02-05 07:47:15 -0600

    Add example code to detect death of parent (.app)

--- main.rb
+++ main.rb
@@ -11,6 +11,10 @@ end
 $x = 0
 while true
     puts "#{$x}. ohai osx!"
+    if Process.ppid == 1
+      puts "Parent process (.app) died, so I'm stopping"
+      break
+    end
     sleep 1
     $x += 1
 end
